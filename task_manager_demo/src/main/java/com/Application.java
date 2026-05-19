9d12c890dc6521c165677a72373b0f26878cf60d
Jash Shah <jash.shah@asint.net>
2026-05-19 16:55:23 +0530

    Add network test

--- task_manager_demo/src/main/java/com/Application.java
+++ task_manager_demo/src/main/java/com/Application.java
@@ -1,4 +1,4 @@
-package com.demo;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(
-                Application.class,
-                args
-        );
-
+        SpringApplication.run(Application.class, args);
     }
 
 }
